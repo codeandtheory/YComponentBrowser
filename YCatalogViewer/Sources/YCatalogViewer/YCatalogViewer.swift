ff40003949d4d1ffd9d2cd45f6275c591fd9a792
PanchamiShenoy <panchami.shenoy@ymedialabs.com>
2022-07-20 11:59:34 +0530

    [CM -754] created swift package YCatalogViewer

---
+++ YCatalogViewer/Sources/YCatalogViewer/YCatalogViewer.swift
@@ -0,0 +1,8 @@
+/// YCatalogViewer struct
+public struct YCatalogViewer {
+    public private(set) var text = "Hello, World!"
+    
+    /// init method to intialize instance of struct
+    public init() {
+    }
+}
