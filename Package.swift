4be001c01fbb2843bab3fd5d22748e3e6b484fc0
Tim Barrett <tim.barrett@ymedialabs.com>
2022-07-21 06:20:16 -0700

    [CM-757] Create a SwiftLint Plugin for a Swift Package

--- Package.swift
+++ Package.swift
@@ -16,20 +16,28 @@ let package = Package(
             targets: ["YCatalogViewer"]
         )
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "SwiftLintBinary",
+            url: "https://github.com/realm/SwiftLint/releases/download/0.47.1/SwiftLintBinary-macos.artifactbundle.zip",
+            checksum: "82ef90b7d76b02e41edd73423687d9cedf0bb9849dcbedad8df3a461e5a7b555"
+        ),
+        .plugin(
+            name: "SwiftLint",
+            capability: .buildTool(),
+            dependencies: ["SwiftLintBinary"]
+        ),
         .target(
             name: "YCatalogViewer",
-            dependencies: []
+            dependencies: [],
+            plugins: ["SwiftLint"]
         ),
         .testTarget(
             name: "YCatalogViewerTests",
-            dependencies: ["YCatalogViewer"]
+            dependencies: ["YCatalogViewer"],
+            plugins: ["SwiftLint"]
         )
     ]
 )
