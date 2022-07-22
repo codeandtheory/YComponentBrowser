02ae26af7d81a9b886dee49f336e61bf883d706c
Mark Pospesel <mark.pospesel@ymedialabs.com>
2022-07-22 16:39:04 +0200

    [CM-772] Add Jazzy config, update README, and complete documentation coverage

--- Sources/YCatalogViewer/YCatalogViewer.swift
+++ Sources/YCatalogViewer/YCatalogViewer.swift
@@ -1,5 +1,6 @@
 /// YCatalogViewer struct
 public struct YCatalogViewer {
+    /// Sample text to display
     public private(set) var text = "Hello, World!"
     
     /// init method to intialize instance of struct
