19727eb8d3e5a3319c3af3ff0a2f64ba1aa3bf3d
PanchamiShenoy <panchami.shenoy@ymedialabs.com>
2022-08-01 18:31:50 +0530

    [CM -767] Created separate file for ContentView

---
+++ Sources/YCatalogViewer/UI Components/GenericViews/ContentView.swift
@@ -0,0 +1,10 @@
+//
+//  ContentView.swift
+//
+//  Created by Y Media Labs on 01/08/22.
+//
+
+import UIKit
+
+/// A `Reusable` and `Populatable` view
+public typealias ContentView = UIView & Reusable & Populatable
