93efc3dd893958ffbfdbe18bd3da134246698204
PanchamiShenoy <panchami.shenoy@ymedialabs.com>
2022-08-03 19:45:37 +0530

    [CM - 767] updated IconographyView with new test cases

--- Tests/YCatalogViewerTests/UIComponentsTest/IconographyViewTest.swift
+++ Tests/YCatalogViewerTests/UIComponentsTest/IconographyViewTest.swift
@@ -8,8 +8,35 @@ import XCTest
 @testable import YCatalogViewer
 
 final class IconographyViewTest: XCTestCase {
-    func testIconographyViewForNSCoder() throws {
+    var view: IconographyView?
+    
+    override func setUp() {
+        super.setUp()
+        
+        view = IconographyView(frame: .init())
+    }
+    
+    override func tearDown() {
+        view = nil
+        
+        super.tearDown()
+    }
+    
+    func testForNSCoder() throws {
         let iconView = IconographyView(coder: NSCoder())
         XCTAssertNil(iconView)
+    }
+    
+    func testPopulatable() throws {
+        XCTAssertNil(view?.image)
+        view?.populate(with: UIImage(systemName: "person.fill") ?? UIImage())
+        XCTAssertNotNil(view?.image)
+    }
+    
+    func testPrepareForReusable() throws {
+        view?.populate(with: UIImage(systemName: "person.fill") ?? UIImage())
+        XCTAssertNotNil(view?.image)
+        view?.prepareForReuse()
+        XCTAssertNil(view?.image)
     }
 }
