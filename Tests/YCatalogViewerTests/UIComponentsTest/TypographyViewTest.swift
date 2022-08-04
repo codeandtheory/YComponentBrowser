54cdbcb37bd84bdd1f310d95855bb5f2a7b77e40
PanchamiShenoy <panchami.shenoy@ymedialabs.com>
2022-08-04 13:43:33 +0530

    [CM -789] updated TypograpyView with protocol implementation in different extension and also added new test cases

--- Tests/YCatalogViewerTests/UIComponentsTest/TypographyViewTest.swift
+++ Tests/YCatalogViewerTests/UIComponentsTest/TypographyViewTest.swift
@@ -8,8 +8,31 @@ import XCTest
 @testable import YCatalogViewer
 
 final class TypographyViewTest: XCTestCase {
+    var typographyView: TypographyView!
+    
+    override func setUp() {
+        super.setUp()
+        typographyView = TypographyView(frame: .init())
+    }
+    
+    override func tearDown() {
+        typographyView = nil
+        super.tearDown()
+    }
+    
     func testTypographyViewForNSCoder() throws {
         let typographyView = TypographyView(coder: NSCoder())
         XCTAssertNil(typographyView)
+    }
+    
+    func testPopulate() throws {
+        XCTAssertNotEqual(typographyView.font, .boldSystemFont(ofSize: 20))
+        typographyView.populate(with: TypographyModel(font: .boldSystemFont(ofSize: 20)))
+        XCTAssertEqual(typographyView.font, .boldSystemFont(ofSize: 20))
+    }
+    
+    func testReuable() throws {
+        typographyView.prepareForReuse()
+        XCTAssertEqual(typographyView.font, UIFont.systemFont(ofSize: 17))
     }
 }
