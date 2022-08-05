2d35255028e1a9788a93cfe281110beb037ad248
PanchamiShenoy <panchami.shenoy@ymedialabs.com>
2022-08-05 12:27:46 +0530

    [CM - 768] removed YCatalogViewer and YCatalogViewerTest files. removed typealias DisplayView and added optional conformance of selectable and highlightable protocols

--- Sources/YCatalogViewer/YCatalogViewer.swift
+++
@@ -1,9 +0,0 @@
-/// YCatalogViewer struct
-public struct YCatalogViewer {
-    /// Sample text to display
-    public private(set) var text = "Hello, World!"
-    
-    /// init method to intialize instance of struct
-    public init() {
-    }
-}
