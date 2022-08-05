2e28721aaa4a1a21ac9ba230ea483886a177a20a
Mark Pospesel <mark.pospesel@ymedialabs.com>
2022-08-05 10:00:59 +0200

    [CM-768] Add SwiftUI previews

--- Sources/YCatalogViewer/UI Components/GenericViews/CatalogDisplayView.swift
+++ Sources/YCatalogViewer/UI Components/GenericViews/CatalogDisplayView.swift
@@ -84,3 +84,40 @@ final class CatalogDisplayView<View: ContentView>: UIView, Populatable, Highligh
         ])
     }
 }
+
+import SwiftUI
+
+struct CatalogDisplayViewContainer: UIViewRepresentable {
+    typealias UIViewType = CatalogDisplayView<ColorView>
+
+    let model: CatalogDisplayView<ColorView>.Model
+
+    init(title: String, detail: String = "", color: UIColor) {
+        self.model = CatalogDisplayView<ColorView>.Model(
+            title: title,
+            detail: detail,
+            displayViewAxis: .horizontal,
+            displayViewModel: color
+        )
+    }
+
+    func makeUIView(context: Context) -> UIViewType {
+        let view = CatalogDisplayView<ColorView>()
+        view.populate(with: model)
+        return view
+    }
+
+    func updateUIView(_ uiView: CatalogDisplayView<ColorView>, context: Context) {}
+}
+
+struct CatalogDisplayViewPreviews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            CatalogDisplayViewContainer(title: "Red", detail: "System", color: .systemRed)
+                .colorScheme(.light)
+            CatalogDisplayViewContainer(title: "Red", detail: "System", color: .systemRed)
+                .colorScheme(.dark)
+        }
+        .previewLayout(.fixed(width: 375, height: 60))
+    }
+}
