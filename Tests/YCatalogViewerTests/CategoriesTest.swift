68c8c11038cdc1b8919be8dc8f12fdb0af86e809
PanchamiShenoy <panchami.shenoy@ymedialabs.com>
2022-09-12 12:39:43 +0530

    [CM-798] Created IconographyCategory and added unit test to test it

---
+++ Tests/YCatalogViewerTests/CategoriesTest.swift
@@ -0,0 +1,23 @@
+//
+//  CategoriesTest.swift
+//
+//  Created by Y Media Labs on 12/09/22.
+//
+
+import XCTest
+@testable import YCatalogViewer
+
+final class CategoriesTest: XCTestCase {
+    func testIconographyCategory() {
+        let model = [
+            CatalogDisplayView<IconographyView>.Model(
+                title: "title1",
+                detail: "detail1",
+                displayViewAxis: .horizontal,
+                displayViewModel: UIImage(systemName: "person.fill") ?? UIImage()
+            )
+        ]
+        let category = IconographyCategory(name: "Icons", models: model)
+        XCTAssertEqual("IconCategory", category.destination.navigationTitle)
+    }
+}
