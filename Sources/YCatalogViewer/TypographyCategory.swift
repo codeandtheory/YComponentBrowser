602b0e90ec416856c0b94154dbaeeab3706680f7
PanchamiShenoy <panchami.shenoy@ymedialabs.com>
2022-09-12 14:43:30 +0530

    [CM-800] Created Typography category and unit test to test it

---
+++ Sources/YCatalogViewer/TypographyCategory.swift
@@ -0,0 +1,22 @@
+//
+//  TypographyCategory.swift
+//
+//  Created by Y Media Labs on 12/09/22.
+//
+
+import Foundation
+
+/// Category for fonts and text
+public struct TypographyCategory: Classification {
+  typealias View = CatalogDisplayView<TypographyView>
+    
+  /// Name of the category
+  public let name: String
+    
+  /// Represents destination of the TypographyCategory
+  public var destination: Destination {
+      CatalogDetailDestination<View>(navigationTitle: "TypographyCategory", models: models)
+  }
+    
+  let models: [View.Model]
+}
